1a217d33e79526342d0231c1399a94ffb8532c38
siemJakub <jakubsiemionowicz@vlo.gliwice.pl>
2025-04-01 12:28:05 +0200

    Add scripted gallery ( to one page only :( )

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,15 +1,75 @@
-const fs = require("FileSystem")
+let page_desc = {
+    "Page1": [ "AGM 65D", "AGM 65E (laser)", "An F-18C armed with Maverick", 
+        "Maveric seeker", "Maveric under the wing" ,"A - 10 firing a Maverick missile" ]
+}
 
-page_tittles = {}
-page_src = {}
-page_desc = ["Maveric seeker", "Maveric under the wing", "AGM 65D", "AGM 65E (laser)"]
+let page_tittles = {
+    "Page1": [ "AGM 65D", "AGM 65E (laser)", "An F-18C armed with Maverick",
+         "Maveric seeker", "Maveric under the wing" ,"A - 10 firing a Maverick missile"] 
+}
+
+let current_index = 1
+let current_faded = 1 // true -> second
 
 document.addEventListener('DOMContentLoaded', () => {
    
-    current_page = "Page"+document.URL[document.URL.length-5]
+    document.querySelector("#gallery_scripted_second_img").addEventListener('click', open_photo_on_clikc);
 
-    for (let i = 0; i < 5; i++) {
-        page_src[i] = "Photos/"+current_page+"/Gallery"
-      }
-
+    document.querySelector("#gallery_btn_right").addEventListener('click', () => {
+        switch_photo(false);
+    })
+    document.querySelector("#gallery_btn_left").addEventListener('click', () => {
+        switch_photo(true);
+    })
 });
+
+function open_photo_on_clikc(e) {
+    if (current_faded == 1) {
+        open(document.getElementById("gallery_scripted_first_img").src);
+    }
+    else {  
+        open(e.target.src);
+    }
+    
+}
+
+function switch_photo(next) {
+    if ( next ) {
+        current_index++;
+    }
+    else {
+        current_index--;
+    }
+    
+    if (current_index > 6) {
+        current_index = 1
+    }
+    else if (current_index <= 0 ) {
+        current_index = 6
+    }
+
+    let current_page = "Page"+document.URL[document.URL.length-6];
+    let path = "Photos/"+current_page+"/Gallery/photo"+current_index+".png";
+
+    let title = document.querySelector('.gallery_scripted h4')
+    let desc = document.querySelector('.gallery_scripted p')
+    
+    title.textContent = page_tittles[current_page][current_index-1]
+    desc.textContent = page_desc[current_page][current_index-1]
+
+    load_photo(path);
+}
+
+function load_photo(path) {
+    let img1 = document.getElementById("gallery_scripted_first_img");
+    let img2 = document.getElementById("gallery_scripted_second_img");
+
+    let images = [img1,img2];
+
+    images[current_faded].src = path;
+
+    images[Math.abs(current_faded - 1)].style.animation = "fadeOut 0.25s linear 1 forwards";
+    images[current_faded].style.animation = "fadeIn 0.25s linear 1 forwards";
+
+    current_faded = Math.abs(current_faded - 1);
+}
--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,6 @@
-
-
 function clock(){
     let ctime = new Date();
     let clock = document.getElementById("clock");
-   // console.log(document.URL[document.URL.length-6])
     clock.textContent = fillZero(ctime.getHours()) + ":" + fillZero(ctime.getMinutes()) + ":" + fillZero(ctime.getSeconds())
 }
 
@@ -19,68 +16,168 @@ function fillZero(timeVar) {
 }
 
 window.global_rec_on = false
+window.hover_elements = []
+//localstorage
 
 function toggle_rec() {
     window.global_rec_on = !window.global_rec_on
+}
+
+function element_hover(element) {
+    switch (element.tagName) {
+        case 'BUTTON':
+            if (element.parentElement.className == "gallery_display" ) {
+                element.style.opacity = "0.7" 
+            }
+            break;
+
+        case 'IMG': 
+            if ( element.parentElement.parentElement.parentElement != null && element.parentElement.parentElement.parentElement.className == 'gallery-flex') {
+                element.style.transform = 'scale(1.10)'
+            }
+
+            if ( element.parentElement.parentElement.tagName == 'NAV') {
+                element.parentElement.style.backgroundColor = "#E5E1DA";
+                element.style.filter = "brightness(0)";
+                element.parentElement.children[1].style.color = "#737373" //napis
+            }
+
+            if ( element.parentElement.parentElement.parentElement.className == 'mainPageBody') {
+                element.parentElement.parentElement.style.transform = 'scale(1.02)';
+            }
+        case 'P' :
+            if (element.parentElement.parentElement.tagName == 'NAV') {
+                element.parentElement.style.backgroundColor = "#E5E1DA"
+                element.parentElement.children[0].style.filter = "brightness(0)";
+                element.style.color = "#737373"
+            }
+
+            break;
+
+        default:
+
+    }
+}
+
+function element_hover_off(element) {
+
+    if (element == null) {return}
+
+    
+
+    switch (element.tagName) {
+        case 'BUTTON':
+            if (element.parentElement.className != null && element.parentElement.className == "gallery_display" ) {
+                element.attributeStyleMap.clear() 
+            }
+            break;
+
+        case 'IMG': 
+            if ( element.parentElement.parentElement.parentElement != null 
+              && element.parentElement.parentElement.parentElement.className == 'gallery-flex') {
+                
+                element.attributeStyleMap.clear()
+            }
+
+            if ( element.parentElement.parentElement.tagName != null 
+              && element.parentElement.parentElement.tagName == 'NAV') {
+
+                element.parentElement.attributeStyleMap.clear()
+                element.attributeStyleMap.clear()
+                element.parentElement.children[1].attributeStyleMap.clear()
+            }
+
+            if ( element.parentElement.parentElement.parentElement.className != null 
+              && element.parentElement.parentElement.parentElement.className == 'mainPageBody') {
+
+                element.parentElement.parentElement.attributeStyleMap.clear()
+            }
+
+        default:
+    }
+}
+
+function add_to_list(element) {
+    if (!window.hover_elements.includes(element) ) {
+        window.hover_elements.push(element);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
     let event = null
     let alpha = 1.0
+    let hover_element = null
+    let allowed_elements = ['BUTTON', 'A', 'IFRAME', 'IMG', 'P']
 
-    document.body.addEventListener('mousemove', (e) => {
-        let canvas = document.querySelector('canvas');
+    document.body.addEventListener('mousemove', (e) =>  {
+        event = e
+
         if(window.global_rec_on) {
-            canvas.style.display = 'block'
-            event = e
-            canvas.style.top = `${e.clientY-50}px`;
-            canvas.style.left = `${e.clientX-50}px`;
-
-            let elements = document.elementsFromPoint(e.clientX, e.clientY);
-            for ( i of elements ) {
-                
-                if( i.tagName == 'BUTTON' || i.tagName == 'A' || i.tagName == 'EFRAME' ) {
-                    alpha = 0.25
-                    
-                    break
-                }
-                else {
-                    alpha = 1
-                }
-            }
-        }
-        else {
-            canvas.style.display = 'none'
-       }
-    }) 
-    
-    document.body.addEventListener('click', (e) => {
-        if ( event != null ) {
             let elements = document.elementsFromPoint(event.clientX, event.clientY);
-            for ( i of elements ) {
-                if (i.id != "canvasRec") {
-                
-                i.click()
-                
-                }
-            }   
+            
+            if( allowed_elements.includes(elements[1].tagName) ) {
+                alpha = 0.25;
+                element_hover(elements[1]); 
+            }
+            else {
+                alpha = 1;
+            }
+
+            if (hover_element != elements[1]) {
+                element_hover_off(hover_element);
+            }
+            hover_element = elements[1];
         }
-        
+    });
+
+    document.body.addEventListener('mouseleave', (e) => {
+
+        // if(window.global_rec_on) {
+
+        //     let elements = document.elementsFromPoint(event.clientX, event.clientY);
+        //     if( i.tagName == 'BUTTON' || i.tagName == 'A' || i.tagName == 'EFRAME' || i.tagName == 'IMG' ) {
+        //         console.log('leave')
+        //         element_hover_off(elements[1])
+        //         //alpha = 1   
+        //     }
+        // }
+    }) 
+
+    document.body.addEventListener('click', (e) => {
+        if ( event != null && window.global_rec_on == true && e.target.tagName == 'CANVAS'  ) {
+            let elements = document.elementsFromPoint(event.clientX, event.clientY);
+            
+            elements[1].click();
+        }
+    }) 
+
+    document.querySelector("#btn_toggle").addEventListener('click', (e) => {   
+        toggle_rec();
     })
 
     let canvas = document.querySelector('canvas');
     let ctx = canvas.getContext('2d');
 
     function frame() {
-         
         clock()
-        canvas.style.opacity = alpha.toString();
-        let time = Date.now();
-        ctx.globalAlpha = 0.5;
-        ctx.fillStyle ="rgba("+(time / 10 % 255)+","+(time / 5 % 255)+","+(time / 2 % 255)+",0.8)";
+       
+        if(window.global_rec_on) {
+           
+            let time = Date.now();
+            canvas.style.top = `${event.clientY-50}px`;
+            canvas.style.left = `${event.clientX-50}px`;
+            canvas.style.opacity = alpha.toString();
+
+            ctx.globalAlpha = 0.5;
+            ctx.fillStyle ="rgba("+(time / 10 % 255)+","+(time / 5 % 255)+","+(time / 2 % 255)+",0.8)";
         
-        ctx.fillRect(0, 0, 100, 100);
+            ctx.fillRect(0, 0, 100, 100);
+            canvas.style.display = 'block'        
+        }
+        else {
+            canvas.style.display = 'none'
+        }
 
         requestAnimationFrame(frame);
     }
